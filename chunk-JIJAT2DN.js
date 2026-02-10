dca2807ee07553eda81c0e6bb75a43be24f9a61d
ivan_pogorenko <ipogorenko@yandex.ru>
2026-02-11 03:58:00 +0700

    Auto-generated commit

---
+++ chunk-JIJAT2DN.js
@@ -0,0 +1,1 @@
+import{Q as n,oa as o,pa as a,sa as s,ta as i}from"./chunk-CZDCVBBI.js";var f=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-task-info"]],standalone:!0,features:[i],decls:2,vars:0,template:function(e,m){e&1&&(o(0,"p"),s(1,"task-info works!"),a())}})};export{f as TaskInfoComponent};
